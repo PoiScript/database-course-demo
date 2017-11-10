49c5e9dddea6384d3eeba4f3bb12c9f54ac8b5ef
PoiScript <poiscript@gmail.com>
2017-11-10 10:23:37 +0800

    feat(api): implement Query for Staff, Supplier

--- api/src/types.rs
+++ api/src/types.rs
@@ -67,6 +67,44 @@ pub struct Staff {
   pub address: String
 }
 
+impl Query for Staff {
+  fn create(&self, conn: &Connection) {
+    &conn.execute(
+      "INSERT INTO staff (id, tele, address) VALUES ($1, $2, $3)",
+      &[&self.id, &self.tele, &self.address]
+    ).unwrap();
+  }
+
+  fn get_all(conn: &Connection) -> Vec<Self> {
+    let mut vec = Vec::new();
+    for row in &conn.query("SELECT id, tele, address FROM staff", &[]).unwrap() {
+      vec.push(Staff {
+        id: row.get(0),
+        tele: row.get(1),
+        address: row.get(2)
+      });
+    }
+    vec
+  }
+
+  fn get_by_id(conn: &Connection, id: i32) -> Self {
+    let rows = &conn.query("SELECT tele, address FROM staff WHERE id = $1", &[&id]).unwrap();
+    let first_row = rows.get(0);
+    Staff {
+      id: first_row.get(0),
+      tele: first_row.get(1),
+      address: first_row.get(2)
+    }
+  }
+
+  fn update(&self, conn: &Connection) {
+    &conn.execute(
+      "UPDATE goods SET tele = $1, address = $2 WHERE id = $3",
+      &[&self.tele, &self.address, &self.id]
+    ).unwrap();
+  }
+}
+
 pub struct Supplier {
   pub id: i32,
   pub zip: i32,
@@ -75,8 +113,89 @@ pub struct Supplier {
   pub address: String
 }
 
+
+impl Query for Supplier {
+  fn create(&self, conn: &Connection) {
+    &conn.execute(
+      "INSERT INTO supplier (id, zip, tele, contact, address) VALUES ($1, $2, $3, $4, $5)",
+      &[&self.id, &self.zip, &self.tele, &self.contact, &self.address]
+    ).unwrap();
+  }
+
+  fn get_all(conn: &Connection) -> Vec<Self> {
+    let mut vec = Vec::new();
+    for row in &conn.query("SELECT id, zip, tele, contact, address FROM supplier", &[]).unwrap() {
+      vec.push(Supplier {
+        id: row.get(0),
+        zip: row.get(1),
+        tele: row.get(2),
+        contact: row.get(3),
+        address: row.get(4)
+      });
+    }
+    vec
+  }
+
+  fn get_by_id(conn: &Connection, id: i32) -> Self {
+    let rows = &conn.query("SELECT zip, tele, contact, address FROM supplier WHERE id = $1", &[&id]).unwrap();
+    let first_row = rows.get(0);
+    Supplier {
+      id: first_row.get(0),
+      zip: first_row.get(1),
+      tele: first_row.get(2),
+      contact: first_row.get(3),
+      address: first_row.get(4)
+    }
+  }
+
+  fn update(&self, conn: &Connection) {
+    &conn.execute(
+      "UPDATE supplier SET zip = $1, tele = $2, contact = $3, address = $4 WHERE id = $5",
+      &[&self.zip, &self.tele, &self.contact, &self.address, &self.id]
+    ).unwrap();
+  }
+}
+
 pub struct Warehouse {
   pub id: i32,
   pub name: String,
   pub address: String
+}
+
+impl Query for Warehouse {
+  fn create(&self, conn: &Connection) {
+    &conn.execute(
+      "INSERT INTO warehouse (id, name, address) VALUES ($1, $2, $3)",
+      &[&self.id, &self.name, &self.address]
+    ).unwrap();
+  }
+
+  fn get_all(conn: &Connection) -> Vec<Self> {
+    let mut vec = Vec::new();
+    for row in &conn.query("SELECT id, name, address FROM warehouse", &[]).unwrap() {
+      vec.push(Warehouse {
+        id: row.get(0),
+        name: row.get(1),
+        address: row.get(2)
+      });
+    }
+    vec
+  }
+
+  fn get_by_id(conn: &Connection, id: i32) -> Self {
+    let rows = &conn.query("SELECT name, address FROM warehouse WHERE id = $1", &[&id]).unwrap();
+    let first_row = rows.get(0);
+    Warehouse {
+      id: first_row.get(0),
+      name: first_row.get(1),
+      address: first_row.get(2)
+    }
+  }
+
+  fn update(&self, conn: &Connection) {
+    &conn.execute(
+      "UPDATE warehouse SET name = $1, address = $2 WHERE id = $3",
+      &[&self.name, &self.address, &self.id]
+    ).unwrap();
+  }
 }
